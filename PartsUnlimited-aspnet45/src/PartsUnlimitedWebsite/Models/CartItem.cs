a1a3a7a06b5a0b9d802ca04d7567307f4d797675
Student1-29608927 <Student1-29608927@LODSPRODMCA.onmicrosoft.com>
2023-03-20 11:47:10 -0700

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //mon premier changement
+        //mon second changement
         [Key]
         public int CartItemId { get; set; }
 
